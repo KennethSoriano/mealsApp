0007de499b2cadd1c3655ba36e7ba16808e10997
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-01-12 13:46:11 -0800

    navigation between two screens

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,14 +3,24 @@ import { FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
-function renderCategoryItem(item) {
-    return (
-        <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>
-    )
-}
 
 
-function CategoriesScreen() {
+
+function CategoriesScreen({navigation}) {
+    function renderCategoryItem(item) {
+        function pressHandler() {
+            navigation.navigate('MealsOverview');
+        }
+    
+        return (
+            <CategoryGridTile 
+                title={itemData.item.title} 
+                color={itemData.item.color} 
+                onPress={pressHandler}
+            />
+        )
+    }
+
     return (
         <FlatList 
             data={CATEGORIES} 
