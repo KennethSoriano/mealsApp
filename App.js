89a2a6d9d6a275c345926f2c13f1e08498621ff2
Kenneth Soriano <Kennethsoriano14017@gmail.com>
2023-01-13 13:50:10 -0800

    setting navigation options dynamically

--- App.js
+++ App.js
@@ -27,7 +27,11 @@ export default function App() {
               title: 'All Categories',
             }}
           />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
+          <Stack.Screen 
+            name="MealsOverview" 
+            component={MealsOverviewScreen} 
+            
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
